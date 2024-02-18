e19cbc171e6d6a6475993de76846836c508f1a9c
Roman Hrdlovic <109904342+appl3m4n@users.noreply.github.com>
2024-02-18 19:35:41 +0100

    x
    x

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask (__name__)
+@app.route ('/')
+def index ():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def getvalue():
+    name = request.form['name']
+    age = request.form['age']
+    db = request.form ['dateofbirth']
+    print(name)
+    return render_template ('pass.html', n=name, age=age, db=db)
+
+if __name__ == '__main__':
+    app.run (debug=True)
