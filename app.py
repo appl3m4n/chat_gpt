850c0cb661e0ee2d2621300c94b7b956b8006a4e
Roman Hrdlovic <109904342+appl3m4n@users.noreply.github.com>
2024-02-20 19:57:36 +0100

    x
    x

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def getvalue():
     return render_template ('index2.html', n=name, age=age,db=db, xx=y)
 
 if __name__ == '__main__':
-    app.run (debug=True)
+    app.run(debug=False, host='0.0.0.0')
